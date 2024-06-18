0866cf814e23398086e9e81e4e2becaccb763b00
Eli <elijames2301@gmail.com>
2024-06-18 14:50:38 -0400

    Write card value

--- blackjack.py
+++ blackjack.py
@@ -6,3 +6,12 @@ card_list = ['Ace', '2','3', '4', '5', '6','7','8', '9', '10'
 
 deck = [(card, suit) for suit in card_categories for card in card_list]
 
+def card_value(card):
+    if card[0] in ['Jack', 'Queen', 'King']:
+        return 10
+    
+    elif card[0] == 'Ace':
+        return 11
+    else:
+        return int(card[0])
+    
